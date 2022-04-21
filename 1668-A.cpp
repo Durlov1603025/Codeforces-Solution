493a6d2bfc2365f14873b2370d8c135bec951638
Durlov1603025 <53161692+Durlov1603025@users.noreply.github.com>
2022-04-21 21:05:18 +0600

    Add files via upload

---
+++ 1668-A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,a,b,diff,ans;
+    cin>>t;
+    while (t--)
+    {
+        cin>>a>>b;
+        if ((a==1||b==1)&&a+b>3)
+        {
+            cout<<-1<<endl;
+        }
+        else
+        {
+            diff = abs(a-b);
+            if(diff%2==0)
+            {
+                diff = diff*2;
+            }
+            else
+            {
+                diff = 2*diff-1;
+            }
+            int c = max(a,b);
+            ans = 2* (c-1) + diff;
+            cout<<ans<<endl;
+        }
+
+        
+    }
+    return 0;
+    
+}
